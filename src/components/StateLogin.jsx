4704e1ffad74756516a672d5874a80e485ecccab
Sandeesh Croos <sandeesh1070@gmail.com>
2026-03-30 23:04:26 +0530

    Handling login with useState and useRef

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,36 @@
+import { useRef } from 'react';
+
+export default function Login() {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log('Login form submitted');
+		console.log('Entered email:', emailRef.current.value);
+		console.log('Entered password:', passwordRef.current.value);
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<h2>Login</h2>
+
+			<div className='control-row'>
+				<div className='control no-margin'>
+					<label htmlFor='email'>Email</label>
+					<input id='email' type='email' name='email' ref={emailRef} />
+				</div>
+
+				<div className='control no-margin'>
+					<label htmlFor='password'>Password</label>
+					<input id='password' type='password' name='password' ref={passwordRef} />
+				</div>
+			</div>
+
+			<p className='form-actions'>
+				<button className='button button-flat'>Reset</button>
+				<button className='button'>Login</button>
+			</p>
+		</form>
+	);
+}
